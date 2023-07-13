4af44bf8c7c2ddae22065cf7e2a9805754b938b3
Constance <nozepastabos@gmail.com>
2023-07-13 17:09:24 +0200

    .h poto

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 16:39:32 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/07/13 16:41:30 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_atoi(const char *str);
+int		ft_strlen(char *s);
+void	ft_putnbr(int nb);
+
+#endif
