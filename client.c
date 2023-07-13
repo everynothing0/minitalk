8ec3c3ad775b9410565fea5a657081fe73b54f98
Constance <nozepastabos@gmail.com>
2023-07-13 17:07:48 +0200

    first commit

---
+++ client.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 16:38:41 by cde-voog          #+#    #+#             */
+/*   Updated: 2023/07/13 16:39:07 by cde-voog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+static void	ft_talker(pid_t pid, char c)
+{
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
+
+static void	ft_head(pid_t pid, char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_talker(pid, s[i]);
+		usleep(100);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+
+	if (argc != 3)
+	{
+		write(1, "\33[91mError: Not enough arguments.\033[0m\n",
+			ft_strlen("\33[91mError: Not enough arguments.\033[0m\n"));
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+	ft_head(pid, argv[2]);
+	return (0);
+}
