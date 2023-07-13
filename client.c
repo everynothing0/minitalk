7a9cab20de0436f49fa24329711dea328220eb81
Constance <nozepastabos@gmail.com>
2023-07-13 17:38:24 +0200

    client

--- client.c
+++ client.c
@@ -6,11 +6,11 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:38:41 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/07/13 16:39:07 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:38:00 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+ #include "minitalk.h"
 
 static void	ft_talker(pid_t pid, char c)
 {
@@ -38,6 +38,41 @@ static void	ft_head(pid_t pid, char *s)
 		usleep(100);
 		i++;
 	}
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = 0;
+	j = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			j = -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	return (res * j);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
 int	main(int argc, char **argv)
