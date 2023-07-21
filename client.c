fbc38a2aff21a49483d1aa7867c0672f8f6c8cd4
Constance <nozepastabos@gmail.com>
2023-07-21 14:12:09 +0200

    euh point dinterrogstion

--- client.c
+++ client.c
@@ -6,92 +6,11 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:38:41 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/07/20 16:29:26 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:08:42 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-/*
-static void	ft_talker(pid_t pid, char c)
-{
-	int	i;
-
-	i = 8;
-	while (i--)
-	{
-		if ((c >> i) & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-	}
-}
-
-static void	ft_head(pid_t pid, char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		ft_talker(pid, s[i]);
-		usleep(100);
-		i++;
-	}
-}
-
-int	ft_atoi(const char *str)
-{
-	int	i;
-	int	j;
-	int	res;
-
-	i = 0;
-	j = 1;
-	res = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			j = -1;
-		i++;
-	}
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		res = res * 10 + (str[i] - 48);
-		i++;
-	}
-	return (res * j);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-	write(1, "\n", 1);
-}
-
-int	main(int argc, char **argv)
-{
-	pid_t	pid;
-
-	if (argc != 3)
-	{
-		write(1, "\33[91mError: Not enough arguments.\033[0m\n",
-			ft_strlen("\33[91mError: Not enough arguments.\033[0m\n"));
-		return (0);
-	}
-	pid = ft_atoi(argv[1]);
-	ft_head(pid, argv[2]);
-	return (0);
-}
-*/
 
 int	ft_atoi(const char *str)
 {
@@ -118,6 +37,16 @@ int	ft_atoi(const char *str)
 	to_return *= sign;
 	return ((int)to_return);
 }
+
+//int	strange(const char *str)
+//{
+//	int		i;
+//
+//	i = 0;
+//	while (str[i] >= 32 && str[i] <= 47)
+//		i++;
+//	return (i);
+//}
 
 void	ft_putchar_fd(char c, int fd)
 {
@@ -200,6 +129,11 @@ void	show_error(int err)
 		ft_putstr_fd("You can send one string \n", 1);
 		exit(1);
 	}
+	//else if (err >= 33 && err <= 47)
+	//{
+		//ft_putstr_fd("Doesn't work with ipic\n", 1);
+	//	exit(1);
+	//}
 }
 
 void	ft_send_data(int data, int pid)
