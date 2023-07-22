d9edf41bb81b34edaab69933acba47a4009d0d49
Constance <nozepastabos@gmail.com>
2023-07-22 14:35:23 +0200

    client

--- client.c
+++ client.c
@@ -6,101 +6,11 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:38:41 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/07/21 14:08:42 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:45:02 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-int	ft_atoi(const char *str)
-{
-	int		i;
-	long	to_return;
-	int		sign;
-
-	to_return = 0;
-	sign = 1;
-	i = 0;
-	while ((str[i] <= 13 && str[i] >= 9) || str[i] == 32)
-		i++;
-	if (str[i] == 43 || str[i] == 45)
-	{
-		if (str[i] == 45)
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		to_return = to_return * 10 + (str[i] - 48);
-		i++;
-	}
-	to_return *= sign;
-	return ((int)to_return);
-}
-
-//int	strange(const char *str)
-//{
-//	int		i;
-//
-//	i = 0;
-//	while (str[i] >= 32 && str[i] <= 47)
-//		i++;
-//	return (i);
-//}
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
-
-	i = 0;
-	if (!fd || !s)
-		return ;
-	while (s[i])
-		ft_putchar_fd(s[i++], fd);
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n == -2147483648)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		ft_putnbr_fd(147483648, fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-		ft_putnbr_fd(n, fd);
-	}
-	else if (n > 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10 + '0', fd);
-	}
-	else if (n < 10)
-	{
-		ft_putchar_fd(n + '0', fd);
-	}
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (*s == (char)c)
-		return ((char *)s);
-	return (NULL);
-}
 
 void	show_error(int err)
 {
@@ -129,11 +39,6 @@ void	show_error(int err)
 		ft_putstr_fd("You can send one string \n", 1);
 		exit(1);
 	}
-	//else if (err >= 33 && err <= 47)
-	//{
-		//ft_putstr_fd("Doesn't work with ipic\n", 1);
-	//	exit(1);
-	//}
 }
 
 void	ft_send_data(int data, int pid)
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:39:32 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/07/13 16:41:30 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/07/21 20:10:12 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@
 # include <stdarg.h>
 
 int		ft_atoi(const char *str);
-int		ft_strlen(char *s);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(char c, int fd);
+char	*ft_strchr(const char *s, int c);
 void	ft_putnbr(int nb);
 
 #endif
