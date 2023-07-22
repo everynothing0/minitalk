5a348aa20ce679010bdf9513636717d1311a1902
Constance <nozepastabos@gmail.com>
2023-07-22 15:50:58 +0200

    alors

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:39:32 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/07/21 20:10:12 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/07/22 14:22:52 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 int		ft_atoi(const char *str);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putnbr_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
 char	*ft_strchr(const char *s, int c);
 void	ft_putnbr(int nb);
 
